0aa49cf9f9910d040ac70a27b71fe552d5bd74f7
Nowak <vnowak@cps.k12.il.us>
2024-04-12 12:07:17 -0500

    second day part 3

--- day2.py
+++ day2.py
@@ -1,71 +1,77 @@
-# Description: This file is for the second day of the python workshop
+# # Description: This file is for the second day of the python workshop
 
 
-# create variables for the following :
-# 1. age
-age = 25 #this is an intger
-# 2. name 
-name = "John" #this is a string
-# 3. song
-song = "Pretty"
-# 4. food
-food = "pizza"
-# 5. number
-number = "13"
-priceOfMovie = 10.99 #this is a float
-# #now include the variables you just made print in the following...
-print(f'''Once upon a time, there was a {age} old coder named {name}. {name} liked to hum the song {song} while coding. It was so annoying that their teammates would throw {food} until {name} would stop singing. Still, {name} was the best coder on the team and could write {number} lines of code every day. Maybe {song} was {name}’s secret power? # No one will ever know. ''')
-# Once upon a time, there was a [age] old coder named [name].
-# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would
-# throw [food] until [name] would stop singing.
-# Still, [name] was the best coder on the team and could write [number] lines of code every day.
-# Maybe [song] was [name]’s secret power?
-# No one will ever know.
+# # create variables for the following :
+# # 1. age
+# age = 25 #this is an intger
+# # 2. name 
+# name = "John" #this is a string
+# # 3. song
+# song = "Pretty"
+# # 4. food
+# food = "pizza"
+# # 5. number
+# number = "13"
+# priceOfMovie = 10.99 #this is a float
+# # #now include the variables you just made print in the following...
+# print(f'''Once upon a time, there was a {age} old coder named {name}. {name} liked to hum the song {song} while coding. It was so annoying that their teammates would throw {food} until {name} would stop singing. Still, {name} was the best coder on the team and could write {number} lines of code every day. Maybe {song} was {name}’s secret power? # No one will ever know. ''')
+# # Once upon a time, there was a [age] old coder named [name].
+# # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would
+# # throw [food] until [name] would stop singing.
+# # Still, [name] was the best coder on the team and could write [number] lines of code every day.
+# # Maybe [song] was [name]’s secret power?
+# # No one will ever know.
 
 
-# What is syntax ? it is a way of writing code
-#What is an algorithm?
-# a set of instructions that are followed to solve 
-#what is python? 
-#name ="John" #python syntax
-# what is a variable? What is a string?
-#varaible holds data 
-#name = "John" string this is defined
-#age = is undefined because it has no value
-# strings are nothing but plain text
-# what does this do?
-print("Giraffe \n academy") 
-# \n makes a new line
-print("giraffe \t academy")
-# \t makes a tab
+# # What is syntax ? it is a way of writing code
+# #What is an algorithm?
+# # a set of instructions that are followed to solve 
+# #what is python? 
+# #name ="John" #python syntax
+# # what is a variable? What is a string?
+# #varaible holds data 
+# #name = "John" string this is defined
+# #age = is undefined because it has no value
+# # strings are nothing but plain text
+# # what does this do?
+# print("Giraffe \n academy") 
+# # \n makes a new line
+# print("giraffe \t academy")
+# # \t makes a tab
 
-# or this
-phrase = "python learning" #this is a string
-print(phrase + "is cool") 
-#this called concatenation or string interpolation
-#what does the + sign do? What is it called?
-
-
+# # or this
+# phrase = "python learning" #this is a string
+# print(phrase + "is cool") 
+# #this called concatenation or string interpolation
+# #what does the + sign do? What is it called?
 
 
-#what if I wanted to get the length of a phrase?
-#phrase = str(phrase)
-print(f'the length of the phrase {len(phrase)}')
-declarationOfInDependece = "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation." 
-#declarationOfInDependece = str(declarationOfInDependece)
-print(f'the length of the declaration is {len(declarationOfInDependece)}')
+
+
+# #what if I wanted to get the length of a phrase?
+# #phrase = str(phrase)
+# print(f'the length of the phrase {len(phrase)}')
+# declarationOfInDependece = "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation." 
+# #declarationOfInDependece = str(declarationOfInDependece)
+# print(f'the length of the declaration is {len(declarationOfInDependece)}')
 #len allows you to find the length on a string
 
 #what if I wanted to make the letters in the variable upper case or lower?
-
-
-
+new_phrase="welcome to day 2 part 3"
+print(len(new_phrase))
+print(new_phrase.upper()) #.upper is a method that makes the string all upper case
+print(new_phrase.lower()) #.lower is a method that makes the string all lower case
+#the purple parentheses are ised to call methods
 
 #what if I wanted to check and see if the phrase was all lower or upper?
-
+print(new_phrase.islower())#returns a boolean true or false 
 
 #What if I wanted to get one letter of the phrase
-
+print(new_phrase[0]) #prints first letter
+print(new_phrase[1])
+print(new_phrase[2])
+print(new_phrase[11])
+print(new_phrase[-1])
 
 # The names you use when creating these labels need to follow a few rules:
 # 1. Names can not start with a number.
@@ -77,7 +83,7 @@ print(f'the length of the declaration is {len(declarationOfInDependece)}')
 
 
 
-# Working with numbers bold text
+# Working with numbers 
 # We'll learn about the following topics:
 # 1. Types of Numbers in Python
 # 2. Basic Arithmetic
@@ -85,21 +91,21 @@ print(f'the length of the declaration is {len(declarationOfInDependece)}')
 
 
 # Addition
-
+print(2 + 2)
 # Subtraction
-
+print(2 - 5)
 # Multiplication
-
+print(2 * 3)
 # Division
-
+print(10 / 2)
 # Modulus
-
+print(10 % 3) #10 iviede by 3 is 3 with a remainder of 1
 # Exponentiation
-
+print(2 ** 3) #2 to the power of 3
 # Floor Division
-
+print(10 // 3) # 10 divided by 3 is 3 with a remainder of 1
 # Order of Operations followed in Python
-
+print(2 * 3 + 1) #7
 # You can use parentheses to specify the order in which you want operations to be performed.
 
 #to do more you need to import special math libraries from python
