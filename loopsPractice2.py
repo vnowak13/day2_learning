a72652663ab5a33de2a995a850d2ad1426fe3434
Nowak <vnowak@cps.k12.il.us>
2024-05-08 12:08:29 -0500

    loops practice 2

---
+++ loopsPractice2.py
@@ -0,0 +1,24 @@
+# listOfNumbers = range(1,101)
+# print(listOfNumbers)
+# for number in listOfNumbers:
+#     # print(number)
+#     if number % 2 == 0:
+#         print(str(number) + " is even")
+# else:
+#     print(str(number) + " is odd")
+
+#create a list of numbers from 1 to 10000
+listofNumbers = range(1,10001)
+#print out the sum of all the numbers in the list
+print(f'the sum of the list is {sum(listofNumbers)}')
+# total = 0
+# for number in listofNumbers:
+#     if number % 2 == 0:
+#         sumOflist = total + number
+#         print(sumOflist)
+#     else:
+#         print("This number is not")
+#if the number is even, add it to the sum
+
+
+
---
+++ task_tickey.py
@@ -0,0 +1,14 @@
+#task:Ticket sale
+ticket = 10
+bought = int(input("How many tickets would you like to buy?"))
+
+#Task: Payment Handling
+person_one = int(input("Amount you are paying, 10 or 20:"))
+person_two = int(input("Amount you are paying, 10 or 20:"))
+one_change = (person_one) - (ticket) 
+print(one_change)
+two_change = (person_two) - (ticket) 
+print(two_change)
+
+#task:Display Total Sales
+print(person_one) + (person_two)
